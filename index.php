2b727be6bf927dec0206aecc67dddb9bc709fbcf
Le Bao Triet <crist.azn@gmail.com>
2013-10-04 05:48:18 +0700

    dad

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php 
+session_start();
 echo 'dsada'
 $_SESSION['avc'] = 'session string';
 ?>
--- test/sub/abc.php
+++ test/sub/abc.php
@@ -1,4 +1,5 @@
 <?php 
+session_start();
 	echo "Should not see this line!";
 	print_r($_SESSION);
 
